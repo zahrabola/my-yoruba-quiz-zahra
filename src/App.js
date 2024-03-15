86a71a92f070ed9e39c6165b1d1e35c7c5cc3687
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-03-15 13:05:17 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { dataLanguage } from "./Data/index";
+///import { FaMicrophone } from "react-icons/fa";
+import ReactAudioPlayer from "react-audio-player";
 
 function App() {
+  const [inputstate, setInputState] = useState("");
+  const [current, setCurrent] = useState(0);
+  const [score, setScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+  const [error, setError] = useState(false);
+
+  const setRandomYoruba = () => {
+    const randomIndex = Math.floor(Math.random() * dataLanguage.length);
+    setCurrent(randomIndex);
+  };
+
+  const handleChange = (event) => {
+    setInputState(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (inputstate.toLowerCase() === dataLanguage[current].english) {
+      setScore(score + 1);
+      setMaxScore(score + 1 > maxScore ? score + 1 : maxScore);
+      setError(false);
+
+      localStorage.setItem("score", score + 1);
+      localStorage.setItem(
+        "maxScore",
+        score + 1 > maxScore ? score + 1 : maxScore
+      );
+    } else {
+      const y = dataLanguage[current].yoruba;
+      const e = dataLanguage[current].english;
+      setError(
+        `That is the wrong answer. The correct answer for ${y} is " ${e} " `
+      );
+      setScore(0);
+      localStorage.setItem("score", 0);
+    }
+    setInputState("");
+    setRandomYoruba();
+  };
+
+  useEffect(() => {
+    setRandomYoruba();
+    setScore(parseInt(localStorage.getItem("score")) || 0);
+    setMaxScore(parseInt(localStorage.getItem("maxScore")) || 0);
+  }, []);
+
+  console.log(dataLanguage[current].audioUrl);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App-Container">
+      <header className="header-app">
+        <h1 className="heading">Yoruba Quiz</h1>
+        <div className="score">
+          <p>
+            {score} / {maxScore}
+          </p>
+        </div>
+        <div className="title">
+          <p>{dataLanguage[current].title}</p>
+        </div>
       </header>
+      <main className="main-app">
+        <div className="word">
+          <h1>{dataLanguage[current].yoruba}</h1>
+        </div>
+        <div className="audio-section">
+          <ReactAudioPlayer
+            src={dataLanguage[current].audioUrl}
+            controls
+            autoPlay
+            className="audio"
+            /*loop
+             autoPlay*/
+          />
+        </div>
+        <div className="section-form">
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={inputstate} onChange={handleChange} className="input-section"/>
+          </form>
+        </div>
+        <div className="message">
+        {!error ? (
+          <div className="clue">Guess the meaning of {dataLanguage[current].yoruba}, quick clue it is a word relating to: {dataLanguage[current].title}</div>
+        ) : (
+          <div className="error">{error}</div>
+        )}
+        </div>
+       
+      </main>
     </div>
   );
 }
 
 export default App;
+/* 
+
+ {error && (
+          <div>
+            <p>{error}</p>
+          </div>
+        )}
+      */
---
+++ src/Data/index.js
@@ -0,0 +1,61 @@
+export const dataLanguage = [
+  { english: "yes", yoruba: "bẹ́ẹ̀ni", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/126b5184f6bd9e246b1c458989956d1c0a66e73f.mp3", title:"Common phrases" },
+  { english: "no", yoruba: "bẹ́ẹ̀kọ́",  audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/b77821a7ef527d0f95a6e139ddd18b249e1e0c45.mp3", title:"Common phrases"},
+  { english: "ok", yoruba: "ó dára", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/9db4e96ab22de260811648f7c93f3248131dddb5.mp3", title:"Common phrases" },
+  { english: "hi", yoruba: "báwo", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/f65650de29ac43514e501da81dee701d97f255f9.mp3", title:"Common phrases" },
+  { english: "thank you", yoruba: "o ṣeun", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/a97a69d14cb04eda8122c2d40e8517278d8f2df6.mp3", title:"Common phrases" },
+  { english: "bye", yoruba: "ó dàbọ̀", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/07f7eb7aea874d4df90f2dbbf73c0d0dd35c70a9.mp3", title:"Common phrases" },
+  { english: "please", yoruba: "jọ̀wọ́", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/caf8e8bb316363afb63233f2a1fad26f47a095da.mp3", title:"Common phrases"  },
+  { english: "and", yoruba: "àti", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/552a3e5e977ac603a3ed813553daa108ea2fff46.mp3", title:"Common phrases"  },
+  { english: "or", yoruba: "tàbí", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/6441eaf2a25b28374e2a2fbc6e11bcf2f4943f54.mp3", title:"Common phrases"  },
+  { english: "this", yoruba: "èyí", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/f0fa6a27f6c85c58d3d5552742f45b883c23d5e5.mp3", title:"Common phrases"  },
+  { english: "it", yoruba: "ó", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/ef3ff9ea20bf08ef36eb64b49f8f375772c2298f.mp3" , title:"Common phrases" },
+  { english: "I like", yoruba: "Mo fẹ́", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/d3ec48e5fe31468aeac86d2c98eb403fc15cfbe9.mp3", title:"Common phrases"  },
+
+
+  { english: "parents", yoruba: "àwọn òbí",  audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/4c3c424055ee316e07e974e230807bbfe1363566.mp3", title:"Family " },
+  { english: "father", yoruba: "bàbá", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/9cb1abe69a3b26c4c22a82c91f470e28df1f8661.mp3", title:"Family" },
+  { english: "grand father", yoruba: "bàbá àgbà", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/ec2a3e85c2b583b7670456fb3209ceac4e84503e.mp3", title:"Family "},
+  { english: "Mother", yoruba: "ìyá", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/9543862b2de255ae66bba43f5f6a864f07ee795d.mp3", title:"Family " },
+  { english: "grand mother", yoruba: "ìyá àgbà", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/5761404ab38b5d44ee1cab0f1d664a1c4e5449ee.mp3", title:"Family " },
+  
+
+
+  { english: "january", yoruba: "Oṣù Kiìíní",  audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/3ddaf8434e1a73ac34a11e091130881d13c1053f.mp3",title:"Month of The Year"},
+  { english: "february", yoruba: "Oṣù Kejì", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/9e6841920ecac1b0b828b4ad4490449c3582dc0a.mp3", title:"Month of The Year" },
+  { english: "march", yoruba: "Oṣù Kẹta", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/0099bf144b958163a37716877c47a19ac5307210.mp3", title:"Month of The Year" },
+  { english: "april", yoruba: "Oṣù Kẹrin", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/0ead69d3852d4ccf6baf048a44a907d67382cf1e.mp3", title:"Month of The Year" },
+  { english: "may", yoruba: "Oṣù Karun", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/a4593a69815c8be09c9ce109aaf86c8c676e5d1b.mp3", title:"Month of The Year" },
+  { english: "june", yoruba: "Oṣù Kẹfà", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/6d98205e78194db7bce9afbf4d0a869af1ccf209.mp3", title:"Month of The Year" },
+  { english: "july", yoruba: "Oṣù Keje", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/1ca0cf7df20857a61d60923491086c0c60607940.mp3", title:"Month of The Year" },
+  { english: "august", yoruba: "Oṣù Kẹjọ", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/32cedcd6741ecef886d318bd8e202db7d2b53f6f.mp3", title:"Month of The Year" },
+  { english: "september", yoruba: "Oṣù Kẹsan", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/ba6af661291bfe0b2b5c71f35ba8f4bcf9520ab1.mp3", title:"Month of The Year" },
+  { english: "october", yoruba: "Oṣù Kẹwàá", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/fc5265485fe80933f7c26739bc1e5698a683b41d.mp3", title:"Month of The Year" },
+  { english: "november", yoruba: "Oṣù Kọkànlá", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/e1e5021b2a535da60cc2c3dc9913ec3986af7a3e.mp3", title:"Month of The Year" },
+  { english: "december", yoruba: "Oṣù Kejìlá", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/e6df5e05bf619ba6f3f959082c52604e2d51a02d.mp3", title:"Month of The Year" },
+  { english: "month", yoruba: "oṣù", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/8bb71cbf812e799fcc57496d23ff084270716af8.mp3", title:"Month of The Year" },
+  { english: "year", yoruba: "ọdún", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/4783e3531244f91abb0cf36541567b1f417cbb27.mp3", title:"Month of The Year" },
+
+
+  { english: "today", yoruba: "òní", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/9541b0d7e6fcedd1e2de6c1325c77133e4209316.mp3", title:"Days of The Week"  },
+  { english: "tomorrow", yoruba: "ọ̀la", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/083434e605d54cd69e9bdd19d556cbd99d6c1030.mp3", title:"Days of The Week"  },
+  { english: "yesterday", yoruba: "àná", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/684a5c2c9f558ab938d6b575a685864511bc1f43.mp3", title:"Days of The Week"  },
+  { english: "monday", yoruba: "Ọjọ́ Ajé", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/99219d6ffb7efc4e5c01af80a262265a36b16ebe.mp3", title:"Days of The Week"  },
+  { english: "tuesday", yoruba: "Ọjọ́ Ìṣẹ́gun", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/7274f22ad6d1dcfd7cf3229c6514e00eaaf22a4f.mp3", title:"Days of The Week"  },
+  { english: "wednesday", yoruba: "Ọjọ́rú", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/c4b5b9a21f0248dccce4f8004bdaab818fa7d954.mp3", title:"Days of The Week"  },
+  { english: "thursday", yoruba: "Ọjọ́bọ̀", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/a5b0b2c06f6fab7f826640b229bfe77b2e6a1e8d.mp3", title:"Days of The Week"  },
+  { english: "friday", yoruba: "Ọjọ́ Ẹtì", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/de4329c9d249962e928500f2e364ecf90a3149b5.mp3", title:"Days of The Week"  },
+  { english: "saturday", yoruba: "Ọjọ́ Àbámẹ́ta", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/f61ee5b6b421bd99f0c2528d95038e89bcf64bad.mp3", title:"Days of The Week"  },
+  { english: "sunday", yoruba: "Ọjọ́ Àìkú", audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/380234883b8f4a4761127b83abf20849696b6561.mp3", title:"Days of The Week"  },
+  { english: "week", yoruba: "Ọ̀sẹ̀" ,audioUrl:"https://content-prod.drops-files.com/v71/sound/mp3/7eb62fd12e3e5fcc51f4c5ffd019c4b0dd3219e5.mp3", title:"Days of The Week"  },
+
+];
+/*{ romanji: 'n', hiragana: 'ん' } 
+
+src="https://content-prod.drops-files.com/v71/sound/mp3/126b5184f6bd9e246b1c458989956d1c0a66e73f.mp3"
+<audio src="https://content-prod.drops-files.com/v71/sound/mp3/b77821a7ef527d0f95a6e139ddd18b249e1e0c45.mp3"></audio>
+https://languagedrops.com/word/en/english/yoruba/
+
+audioUrl:"", title:"" 
+https://languagedrops.com/word/en/english/yoruba/topics/my_family/                                             
+*/
